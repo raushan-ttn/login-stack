cc6909569fae56b3326231da0ef0f3220d4def9d
Raushan <raushan.tiwari@tothenew.com>
2025-08-07 23:28:15 +0530

    Google login

--- backend_express_ts/src/config/passport-setup.ts
+++ backend_express_ts/src/config/passport-setup.ts
@@ -36,7 +36,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-      callbackURL: '/auth/google/callback',
+      callbackURL: `${process.env.BASE_URL}/api/v1/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
